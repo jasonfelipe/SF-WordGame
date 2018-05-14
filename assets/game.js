4bf6c8557d7ac9fef5785bedc12b704f98ab4f9d
Legend <jasonrfelipe@gmail.com>
2018-05-13 18:21:45 -0700

    updated javascript

--- assets/game.js
+++ assets/game.js
@@ -19,12 +19,26 @@ var characters = [
 ]
 
 
-
+//This picks a random word from characters
 var word = characters[Math.floor(Math.random() * characters.length)];
 
+//This sets the amount of letters as "_" from the amount from a character
 var answerArray = [];
-for (var i = 0; i < characters.length; i++) {
+for (var i = 0; i < word.length; i++) {
  answerArray[i] = "_";
 }
-var remainingLetters = characters.length;
 
+//This sets up the players progress towards guessing the word
+var remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+    // Game code goes here
+    answerArray.join("");  // Show the player their progress
+    // Take input from the player
+    // Update answerArray and remainingLetters for every correct guess
+   }
+
+function guess () {
+    var g = document.getElementById("charactername");
+    g.value = g.value.toLowerCase();
+}
