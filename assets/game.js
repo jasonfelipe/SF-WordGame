79ce3b1c059cc07d2d3782d53a0431d929df98d5
Legend <jasonrfelipe@gmail.com>
2018-05-14 18:41:50 -0700

    added javascript

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,6 @@
 
 function gamestart() {
-    
-    
+
 
     var characters = [
         "ryu",
@@ -22,29 +21,43 @@ function gamestart() {
         "feilong",
         "deejay"
     ];
-    
-document.getElementById("gamestart").innerHTML ="Type a letter to begin!"
-document.getElementById("userInput").innerHTML ="You just typed "
 
-var userText = document.getElementById("userGuess");
+    document.getElementById("gamestart").innerHTML = "Type a letter to begin!"
+    document.getElementById("userInput").innerHTML = "You just typed "
 
-document.onkeyup = function(event) {
-    userText.textContent = event.key;
-};
+    var userGuess = document.getElementById("userGuess");
+
+    document.onkeyup = function (event) {
+        userGuess.textContent = event.key;
+    };
 
 
-//This picks a random word from characters
+    //This picks a random word from characters
     var word = characters[Math.floor(Math.random() * characters.length)];
 
 
-//This sets the amount of letters as "_" from the chosen character
+    //This sets the amount of letters as "_" from the chosen character
     var answerArray = [];
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = "_";
     };
-//This sets up the players progress towards guessing the word
-    var remainingLetters = word.length;
 
+    //This sets up the players progress towards guessing the word
+    var remainingLetters = word.length;
+    document.getElementById("answer").innerHTML = answerArray.join("");
+
+
+        if (userGuess != word) {
+            document.getElementById("gamestart").innerHTML = "You got it wrong!"
+        }
+        else {
+            for (var j = 0; j < word.length; j++) {
+                if (word[j] === userGuess) {
+                    answerArray[j] = userGuess;
+                    remainingLetters--;
+                }
+            }
+        }
     
 
-};
+}
