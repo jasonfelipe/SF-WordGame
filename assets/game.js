702f6055767cdca24991d834f9b1e49a81542be4
Legend <jasonrfelipe@gmail.com>
2018-05-16 15:47:45 -0700

    added some more game logic

--- assets/game.js
+++ assets/game.js
@@ -1,62 +1,55 @@
+var characters = [
+    "ryu",
+    "vega",
+    "bison",
+    "ken",
+    "cammy",
+    "balrog",
+    "honda",
+    "akuma",
+    "zangief",
+    "guile",
+    "dhalsim",
+    "sagat",
+    "blanka",
+    "chunli",
+    "thawk",
+    "feilong",
+    "deejay"
+];
+
+var alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+];
+
+
 
 function gamestart() {
-
-    var characters = [
-        "ryu",
-        "vega",
-        "bison",
-        "ken",
-        "cammy",
-        "balrog",
-        "honda",
-        "akuma",
-        "zangief",
-        "guile",
-        "dhalsim",
-        "sagat",
-        "blanka",
-        "chunli",
-        "thawk",
-        "feilong",
-        "deejay"
-    ];
-
-    var alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z'
-    ]
-
-    //this keeps track of wins, losses, and lives
-    var wins = 0
-    var losses = 0
-    var tries = 3
-
-
-
     //This picks a random word from characters
     var word = characters[Math.floor(Math.random() * characters.length)];
 
@@ -76,16 +69,30 @@ function gamestart() {
     //TIPS: the variable of event.key won't work unless the parameter is met inside the brackets.
     //What I mean by this is that userGuess = event.key has to be inside for this to work.
     document.getElementById("gamestart").innerHTML = "Type a letter to begin!"
-    // document.onkeyup = function (event) {
-    //     var userGuess = event.key
-    //     document.getElementById("userInput").innerHTML = "You just guessed " + userGuess
-
 
     document.onkeyup = function (event) {
         var userGuess = event.key
-        document.getElementById("userInput").innerHTML = "You just guessed " + userGuess
+            if (alphabet.indexOf(userGuess) > -1) { 
+                document.getElementById("userInput").innerHTML = "You guessed " + userGuess
+                
+                for (var j = 0; j < word.length; j++) {
+                    if (word[j] === userGuess) {
+                        answerArray[j] = userGuess;
+                        remainingLetters--;
+                    }      
+                }
+            }
+
+        
+            else {
+                document.getElementById("userInput").innerHTML = "Please input a letter."
+
+           
 
             }
 
-        }
+        }        
+
+}
+        
 
