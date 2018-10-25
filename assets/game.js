dddbc93116a3e6b919ca24d22d99c4003a91e0da
Legend <jasonrfelipe@gmail.com>
2018-10-25 08:42:09 -0700

    updated code, added loss images, and added game state to prevent button presses after win/loss

--- assets/game.js
+++ assets/game.js
@@ -2,91 +2,49 @@
 
 
 //audio files
-var gamestartaudio = new Audio ("assets/audio/characterselect.mp3")
-var audioLose = new Audio ('assets/audio/lose.mp3')
-var versus = new Audio ('assets/audio/versus.mp3')
-var winaudio = new Audio ('assets/audio/win.mp3')
+var gamestartaudio = new Audio("assets/audio/characterselect.mp3")
+var audioLose = new Audio('assets/audio/lose.mp3')
+var versus = new Audio('assets/audio/versus.mp3')
+var winaudio = new Audio('assets/audio/win.mp3')
 
 //GOOGLE CHROME DOESN'T WANT TO AUTOPLAY BECAUSE IT INTERNAL SETTINGS
 
-//the words
-var characters = [
-    "ryu",
-    "ken",
-    "cammy",
-    "balrog", 
-    "honda",
-    "zangief",
-    "guile",
-    "dhalsim",
-    "sagat",
-    "blanka",
-    "chunli",
-    "thawk",
-    "feilong",
-    "deejay"
-];
+//the words/assets
 
-//I don't know what to do with these.
-var characterwin = [
-    {name: "ryu", image: 'assets/images/ryu.gif'},
-    {name: "ken", image: 'assets/images/ken.gif'},
-    {name: "cammy", image: 'assets/images/cammy.gif'},
-    {name: "balrog", image:'assets/images/balrog.gif'},
-    {name: "honda", image: 'assets/images/honda.gif'},
-    {name: "zangief", image: 'assets/images/zangief.gif'},
-    {name: "guile", image: 'assets/images/guile.gif'},
-    {name: "dhalsim", image: 'assets/images/dhalsim.gif'},
-    {name: "sagat", image: 'assets/images/sagat.gif'},
-    {name: "blanka", image: 'assets/images/blanka.gif'},
-    {name: "chunli", image: 'assets/images/chunli.gif'},
-    {name: "thawk", image: 'assets/images/thawk.gif'},
-    {name: "feilong", image: 'assets/images/feilong.gif'},
-    {name: "deejay", image: 'assets/images/deejay.gif'}
+var character = [
+    { name: "ryu", win: 'assets/images/ryu.gif', lose: 'assets/images/ryulose.gif'},
+    { name: "ken", win: 'assets/images/ken.gif', lose: 'assets/images/kenlose.gif'},
+    { name: "cammy", win: 'assets/images/cammy.gif', lose: 'assets/images/cammylose.gif' },
+    { name: "balrog", win: 'assets/images/balrog.gif', lose: 'assets/images/balroglose.gif' },
+    { name: "honda", win: 'assets/images/honda.gif', lose: 'assets/images/hondalose.gif' },
+    { name: "zangief", win: 'assets/images/zangief.gif', lose: 'assets/images/zangieflose.gif' },
+    { name: "guile", win: 'assets/images/guile.gif', lose: 'assets/images/guilelose.gif' },
+    { name: "dhalsim", win: 'assets/images/dhalsim.gif', lose: 'assets/images/dhalsimlose.gif' },
+    { name: "sagat", win: 'assets/images/sagat.gif', lose: 'assets/images/sagatlose.gif' },
+    { name: "blanka", win: 'assets/images/blanka.gif', lose: 'assets/images/blankalose.gif' },
+    { name: "chunli", win: 'assets/images/chun-li.gif', lose: 'assets/images/chun-lilose.gif' },
+    { name: "thawk", win: 'assets/images/thawk.gif', lose: 'assets/images/thawklose.gif' },
+    { name: "feilong", win: 'assets/images/feilong.gif', lose: 'assets/images/feilonglose.gif' },
+    { name: "deejay", win: 'assets/images/deejay.gif', lose: 'assets/images/deejaylose.gif' }
 ]
-//I dont undertand
-console.log(characterwin[{name}])
 
-//this is to record which keys were pressed
-var alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-];
+//Game is set to false at the start to prevent any accidental key presses.
+let gameState = false;
 
 //THE CHOSEN!
 var chosenCharacter = "";
-//the letters inside the character.  array
+//the letters inside the character 
 var lettersinCharacter = [];
-//number of blanks in character word. array
-var characterBlanks
-//holds right letters. array
+//number of blanks in character word 
+var characterBlanks =[];
+//holds right letters 
 var answersArray = [];
-//holds wrong letters. array
+//holds wrong letters 
 var wrongLetter = [];
+
+//holds the guessed letters regardless of right or wrong. (actually records any keypress)
+let guessedLetters = ["Alt", 'Control', 'Space'];
+
 
 //the wins and losses, counter variables
 var wins = 0;
@@ -94,87 +52,49 @@ var losses = 0;
 var guessesleft = 5;
 var rightGuess = 0;
 
-//--------------------RESET FUNCTION-------------------------------
+//if gamestate is true, keypresses should work.
 
-function reset() 
-{
-
-    //resets theme after win/lose.
-    versus.play();
-        if (gamestartaudio.play) {
-        gamestartaudio.pause();
-        }else{
-        gamestartaudio.currentTime = 0
+//when a key is pressed down this should be recorded.
+document.onkeydown = function (event) {
+    if (gameState) {
+        var guess = event.key
+        if (!guessedLetters.includes(guess)) {
+            guessedLetters.push(guess);
         }
-    audioLose.pause();
-
-
-    document.getElementById("gamestart").innerHTML = "Type a letter to begin!"
-    document.getElementById('button').innerHTML = "RESET"
-
-    //reset code
-    chosenCharacter = characters[Math.floor(Math.random() *characters.length)];
-    lettersinCharacter = chosenCharacter.split(''); 
-    characterBlanks = lettersinCharacter.length;
-    
-    //reset variables
-    letterGuessed = 0;
-    rightGuess = 0;
-    guessesleft = 5;
-    wrongLetter = [];
-    answersArray = [];
-    alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z'
-    ];
-    test=false;
-    gamestart();
+        else {
+        }
+        compareLetters(guess);
+        winLose();
+    }
 }
-function gamestart() 
-{
+
+//=================
+//~~ GAME START ~~
+//=================
+
+function gamestart() {
+    gameState = true;
+
+    console.log(gameState);
+
+    //plays our audio file.
     versus.play();
-        if (gamestartaudio.play) {
+    if (gamestartaudio.play) {
         gamestartaudio.pause();
-        }else{
+    } else {
         gamestartaudio.currentTime = 0
-        }
+    }
     audioLose.pause();
-
     document.getElementById('characterimage').src = "assets/images/ssf2poster.jpg";
-
     document.getElementById("gamestart").innerHTML = "Type a letter to begin!"
     document.getElementById('button').innerHTML = "RESET"
 
     //This picks a random word from characters
-    chosenCharacter = characters[Math.floor(Math.random() * characters.length)];
+    chosenCharacter = character[Math.floor(Math.random() * character.length)].name
 
     //split allows for the letters to be their own string
-    lettersinCharacter = chosenCharacter.split(''); 
-    
+    lettersinCharacter = chosenCharacter.split('');
+
     //This gives the number of blank spaces 
     characterBlanks = lettersinCharacter.length;
 
@@ -184,35 +104,7 @@ function gamestart()
     guessesleft = 5;
     wrongLetter = [];
     answersArray = [];
-    alphabet = [
-        'a',
-        'b',
-        'c',
-        'd',
-        'e',
-        'f',
-        'g',
-        'h',
-        'i',
-        'j',
-        'k',
-        'l',
-        'm',
-        'n',
-        'o',
-        'p',
-        'q',
-        'r',
-        's',
-        't',
-        'u',
-        'v',
-        'w',
-        'x',
-        'y',
-        'z'
-    ];
-
+    guessedLetters = [];
 
     //for blanks
     for (var i = 0; i < characterBlanks; i++) {
@@ -226,55 +118,84 @@ function gamestart()
     document.getElementById("wrong").innerHTML = wrongLetter;
 }
 
+//=================
+//~~~~~ RESET ~~~~~
+//=================
+function reset() {
 
-    function compareLetters(userGuess) {
-        var userGuess = event.key //event is something the browser regisers
-                                  //the browser 
-        if (chosenCharacter.indexOf(userGuess) > -1) //user input check
-        {
-            for (var i = 0; i < characterBlanks; i++) //this is the loop that checks blanks
-            {
-                if (lettersinCharacter[i] === userGuess)//this fills in user's correct letter into the array
-                {
-                    rightGuess++;
-                    answersArray[i] = userGuess;
-                    document.getElementById('answer').innerHTML = answersArray.join('');
-                }
+    gameState = true;
+
+    //resets theme after win/lose.
+    versus.play();
+    if (gamestartaudio.play) {
+        gamestartaudio.pause();
+    } else {
+        gamestartaudio.currentTime = 0
+    }
+    audioLose.pause();
 
 
-            }
-        }
-            //wrong stuff
-        else 
-            {
-                wrongLetter.push(userGuess);
-                guessesleft--; 
-                document.getElementById("guessesleft").innerHTML =  "Guesses Left: " + guessesleft;
-                document.getElementById("wrong").innerHTML = "Wrong Letters <br><br>" + wrongLetter;
+    document.getElementById("gamestart").innerHTML = "Type a letter to begin!"
+    document.getElementById('button').innerHTML = "RESET"
 
-            }
+    //reset code
+    chosenCharacter = character[Math.floor(Math.random() * character.length)].name
+    lettersinCharacter = chosenCharacter.split('');
+    characterBlanks = lettersinCharacter.length;
+    //reset variables
+    letterGuessed = 0;
+    rightGuess = 0;
+    guessesleft = 5;
+    guessedLetters = [];
+    wrongLetter = [];
+    answersArray = [];
+    test = false;
+    gamestart();
 }
-function winLose ()
-{
-    if (rightGuess === characterBlanks)
-        {
-            wins++;
-            document.getElementById('wins').innerHTML = "Wins " + wins;
-            document.getElementById('answer').innerHTML = chosenCharacter.toUpperCase();
-            document.getElementById('gamestart').innerHTML = "YOU WIN! <br><br> Press CONTINUE to play again!"
-            document.getElementById('button').innerHTML = "CONTINUE?"
-            winaudio.play();
-            console.log(chosenCharacter);
-            
 
-            for (var p = 0; p < characterwin.length; p++){ //Ok remember, double or triple equals is a comparison
-                                                            // one equals assigns a value!!
-                if (characterwin[p].name == chosenCharacter){
-                     document.getElementById('characterimage').src = characterwin[p].image;
-                } 
+
+function compareLetters(userGuess) {
+    if (chosenCharacter.indexOf(userGuess) > -1 || !guessedLetters.includes(userGuess)) //user input check
+    {
+        for (var i = 0; i < characterBlanks; i++) //this is the loop that checks blanks
+        {
+            if (lettersinCharacter[i] === userGuess)//this fills in user's correct letter into the array
+            {
+                rightGuess++;
+                answersArray[i] = userGuess;
+                document.getElementById('answer').innerHTML = answersArray.join('');
+            }
+
+
+        }
+    }
+    //wrong stuff
+    else {
+        wrongLetter.push(userGuess);
+        guessesleft--;
+        document.getElementById("guessesleft").innerHTML = "Guesses Left: " + guessesleft;
+        document.getElementById("wrong").innerHTML = "Wrong Letters <br><br>" + wrongLetter;
+
+    }
+}
+function winLose() {
+    if (rightGuess === characterBlanks) {
+        wins++;
+        document.getElementById('wins').innerHTML = "Wins " + wins;
+        document.getElementById('answer').innerHTML = chosenCharacter.toUpperCase();
+        document.getElementById('gamestart').innerHTML = "YOU WIN! <br><br> Press CONTINUE to play again!"
+        document.getElementById('button').innerHTML = "CONTINUE?"
+        winaudio.play();
+        gameState = false;
+
+
+        for (var p = 0; p < character.length; p++) {
+            if (character[p].name == chosenCharacter) {
+                document.getElementById('characterimage').src = character[p].win;
             }
         }
-    else if (guessesleft === 0){
+    }
+    else if (guessesleft === 0) {
         losses++;
         document.getElementById("losses").innerHTML = "Losses " + losses;
         document.getElementById("answer").innerHTML = chosenCharacter.toUpperCase();
@@ -283,33 +204,15 @@ function winLose ()
         gamestartaudio.pause();
         audioLose.currentTime = 0; //resets the lose theme to 0
         audioLose.play();
+        gameState = false;
 
+        for (var p = 0; p < character.length; p++) {
+            if (character[p].name == chosenCharacter) {
+                document.getElementById('characterimage').src = character[p].lose;
+            }
         }
-    }
 
-
-//Not sure how I can improve this...
-function winlossReset() {
-    if (guessesleft <= -1) 
-    {
-        reset();
     }
 }
 
-document.onkeyup = function(event)
-{
-	test = true;
-	var letterGuessed = event.key;
-	for(var i = 0; i < alphabet.length; i++)
-	{	
-		if(letterGuessed === alphabet[i] && test === true)
-		{
-			var spliceDword = alphabet.splice(i,1);
-			compareLetters(letterGuessed);
-            winlossReset(); //supposed to reset if user presses any key after a loss, 
-                         //but if user presses a key that's correct.... well... it breaks the game.
-            winLose();
-		}
-	}		
-		
-}
+
